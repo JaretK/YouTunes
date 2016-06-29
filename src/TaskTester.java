6ee5b454284cac99a6b767ece3734954459234a8
JaretK <jaret.karnuta@gmail.com>
2016-06-29 01:08:14 -0400

    GUI and static validation improvements

--- src/TaskTester.java
+++ src/TaskTester.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.concurrent.Task;
 import javafx.stage.Stage;
 
 public class TaskTester extends Application {
@@ -9,25 +8,19 @@ public class TaskTester extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		String[] ids = new String[] { "NMXW9Q-yAAw", "uozgE83uG4o",
+				"eKH46ff1roc", "VgKFmC_YgyU", "aADqFviP7wU", "rabOaM2kh9c",
+				"6nRy10_iOPc", "uUo6r0EWycw", "f3WxjIJRKXE", "Y8rnmRYHHwI" };
 		long init = System.currentTimeMillis();
-		YtDownloader dl = new YtDownloader();
-		Task<String> t = dl.updateTitlesTask("SHHknspIQ8Q");
-		Thread thread = new Thread(t);
-		thread.start();
-		thread.join();
-		for (int i = 0; i < 10; i++){
-		long mid = System.currentTimeMillis();
-
-		YoutubeValidator.validateId("SHHknspIQ8Q");
-		YoutubeValidator.getVideoTitle("SHHknspIQ8Q");
-		
-		long end = System.currentTimeMillis();
-		System.out.println((end-mid));
+		for (String ele : ids) {
+			System.out.println(YoutubeValidator.validateId(ele));
+			System.out.println(YoutubeValidator.getVideoTitle(ele));
 		}
+		long end = System.currentTimeMillis();
+		System.out.println(end - init);
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 }
--- src/YoutubeValidator.java
+++ src/YoutubeValidator.java
@@ -1,5 +1,7 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,17 +20,18 @@ import com.google.api.services.youtube.model.VideoSnippet;
 
 public class YoutubeValidator {
 
-	//file containing the api key used for youtube api requests
+	// file containing the api key used for youtube api requests
 	private static final String PROPERTIES_FILENAME = "youtube.properties";
-	//identifier in the properties file
+	// identifier in the properties file
 	private static final String API_KEY_ID = "youtube.apikey";
 
 	private static YouTube youtube;
 	private static String api_key;
-	
-	//Map for storing ytid, snippet mappings
+
+	// Map for storing ytid, snippet mappings
 	private static Map<String, VideoSnippet> cache;
 
+	// initialize all accessed variables
 	static {
 		HttpRequestInitializer HttpInit = new HttpRequestInitializer() {
 			public void initialize(HttpRequest arg0) throws IOException {
@@ -42,13 +45,15 @@ public class YoutubeValidator {
 
 	/**
 	 * Loads api key from the properties file into the api_key variable
+	 * 
 	 * @return
 	 */
 	private static String loadApiKey() {
 		Properties properties = new Properties();
+		String basePath = classFilePath();
 		try {
-			InputStream in = YoutubeValidator.class
-					.getResourceAsStream("Resources/" + PROPERTIES_FILENAME);
+			InputStream in = new FileInputStream(basePath + "Resources/"
+					+ PROPERTIES_FILENAME);
 			properties.load(in);
 		} catch (IOException e) {
 			System.err.println("Error reading " + PROPERTIES_FILENAME + " : "
@@ -60,7 +65,29 @@ public class YoutubeValidator {
 
 	/**
 	 * 
-	 * @param ytid YouTube unique ID
+	 * @return The file path of the compiled program or the execution
+	 *         environment (pre-deploy, testing, etc)
+	 */
+	public static String classFilePath() {
+		// workaround for disallowance of this keyword in static method
+		// get the method handle of this class and then lookup the current
+		// execution class, which is static
+		String in = MethodHandles.lookup().lookupClass().getProtectionDomain()
+				.getCodeSource().getLocation().getPath();
+		in = in.replaceAll("/bin", "");
+		in = in.replaceAll("%20", " ");
+		final String app = ".app/Contents/";
+		if (in.contains(app)) {
+			int index = in.indexOf(app);
+			in = in.substring(0, index + app.length());
+		}
+		return in;
+	}
+
+	/**
+	 * 
+	 * @param ytid
+	 *            YouTube unique ID
 	 * @return True iff the id maps to one video
 	 */
 	public static boolean validateId(String ytid) {
@@ -71,8 +98,8 @@ public class YoutubeValidator {
 			VideoListResponse response = video.execute();
 			List<Video> responseList = response.getItems();
 
-			//ids are unique
-			if (responseList.size() == 1){
+			// ids are unique
+			if (responseList.size() == 1) {
 				cache.put(ytid, responseList.get(0).getSnippet());
 				return true;
 			}
@@ -81,16 +108,17 @@ public class YoutubeValidator {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 
-	 * @param ytid YouTube unique video ID
+	 * @param ytid
+	 *            YouTube unique video ID
 	 * @return title of the video
 	 */
-	public static String getVideoTitle(String ytid){
-		//cache to improve performance
-		//validateID is called first
-		if (cache.containsKey(ytid)){
+	public static String getVideoTitle(String ytid) {
+		// cache to improve performance
+		// validateID should be called first to benefit from performance boost
+		if (cache.containsKey(ytid)) {
 			return cache.get(ytid).getTitle();
 		}
 		try {
@@ -99,7 +127,7 @@ public class YoutubeValidator {
 			video.setId(ytid);
 			VideoListResponse response = video.execute();
 			List<Video> responseList = response.getItems();
-			if (responseList.size() == 1){
+			if (responseList.size() == 1) {
 				VideoSnippet snippet = responseList.get(0).getSnippet();
 				return snippet.getTitle();
 			}
